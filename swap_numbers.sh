ebfab649fbcd4b9e64b7abc930ef4c6b84bc1e9d
Moulidharan <smoulidharanmech@gmail.com>
2022-11-03 14:34:46 +0530

    [mouli]swaping numbers

---
+++ swap_numbers.sh
@@ -0,0 +1,11 @@
+#! /bin/bash -x
+
+read -p "Enter the number : " num1
+read -p "Enter the number : " num2
+
+temp=$num1
+first=$num2
+second=$temp
+
+echo "Before swap First : $temp , Second = $first "
+echo "After swap First : $first , Second = $second "
